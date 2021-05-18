0bfb32bc28c74d28fbc0fd8a5b0c06e9db3ff54d
Jasmina Jovanovic <84245799+sequencesimilarity@users.noreply.github.com>
2021-05-18 16:27:28 +0200

    Add files via upload

---
+++ RPF_load_data.r
@@ -0,0 +1,24 @@
+#set the working directory
+setwd("path")
+
+# 3 mtDNA sequences: Human, Chimpanzee, Rabbit for dn repeat type
+load("RData_mitoh_3seq")
+
+
+# mtDNA sequences dn repeat type
+load("RData_mitoh_dn")
+
+# mtDNA sequences dc repeat type
+load("RData_mitoh_dc")
+
+# mtDNA sequences in repeat type
+load("RData_mitoh_in")
+
+# mtDNA sequences ic repeat type
+load("RData_mitoh_ic")
+
+# test data for 383 RNA viruses dn repeat type
+load("RData_viruses_dn")
+
+# test data for 383 RNA viruses dc repeat type
+load("RData_viruses_dc")
